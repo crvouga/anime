c6811e4e144e4eb413f00aca9534c182c4fd0401
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2022-11-17 13:48:18 -0700

    added more data to page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,3 @@
-<script setup>
-import PosterCard from "../components/PosterCard.vue";
-</script>
-
-<template>
-  <Layout>
-    <div class="container">
-      <div class="row p-0">
-        <div
-          class="col-6 col-sm-4 col-md-3 col-lg-2 p-2"
-          v-for="media in $page.anime.Page.media"
-          :key="media.id"
-        >
-          <g-link class="link" :to="`/anime/${media.id}`">
-            <PosterCard
-              :src="media.coverImage.extraLarge"
-              :title="media.title.english ?? media.title.native"
-              :subtitle="media.seasonYear"
-            />
-          </g-link>
-        </div>
-      </div>
-    </div>
-  </Layout>
-</template>
-
 <page-query>
 {
   anime {
@@ -43,3 +17,30 @@ import PosterCard from "../components/PosterCard.vue";
   }
 }
 </page-query>
+
+<script setup>
+import PosterCard from "../components/PosterCard.vue";
+const allMedia = ($page) => $page.anime.Page.media;
+</script>
+
+<template>
+  <Layout>
+    <div class="container">
+      <div class="row p-0">
+        <div
+          class="col-6 col-sm-4 col-md-3 col-lg-2 p-2"
+          v-for="media in allMedia($page)"
+          :key="media.id"
+        >
+          <g-link class="link" :to="`/anime/${media.id}`">
+            <PosterCard
+              :src="media.coverImage.extraLarge"
+              :title="media.title.english ?? media.title.native"
+              :subtitle="media.seasonYear"
+            />
+          </g-link>
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
--- src/templates/Anime.vue
+++ src/templates/Anime.vue
@@ -1,38 +1,5 @@
-<script setup>
-import AspectRatio from "../components/AspectRatio.vue";
-import ReadMore from "../components/ReadMore.vue";
-
-const media = ($page) => $page.anime.Page.media[0];
-const bannerImage = ($page) => media($page)?.bannerImage ?? "";
-const title = ($page) =>
-  media($page).title.english ?? media($page).title.native;
-const startYear = ($page) => media($page).startDate.year;
-const description = ($page) => media($page).description;
-</script>
-
-<template>
-  <Layout>
-    <main class="container p-0">
-      <AspectRatio w="21" h="9">
-        <img class="w-100 h-100 object-cover" :src="bannerImage($page)" />
-      </AspectRatio>
-      <section class="container">
-        <h1 class="mb-0">{{ title($page) }}</h1>
-        <p class="h6 text-muted">{{ startYear($page) }}</p>
-        <ReadMore class="text-muted" :content="description($page)" />
-      </section>
-    </main>
-  </Layout>
-</template>
-
-<style scoped>
-.object-cover {
-  object-fit: cover;
-}
-</style>
-
 <page-query>
-query ($id: Int) {
+query($id: Int) {
   anime {
     Page {
       media(id: $id) {
@@ -42,12 +9,95 @@ query ($id: Int) {
         startDate {
           year
         }
+        endDate {
+          year
+        }
         title {
           english
           native
         }
+        episodes
+        genres
       }
     }
   }
 }
+
 </page-query>
+
+<script setup>
+import AspectRatio from "../components/AspectRatio.vue";
+import ReadMore from "../components/ReadMore.vue";
+
+/**
+ *
+ * @typedef Title
+ * @type {object}
+ * @prop english {string}
+ * @prop native {string}
+ *
+ * @typedef Date
+ * @type {object}
+ * @prop year {number | null}
+ *
+ * @typedef Media
+ * @type {object}
+ * @prop {number} id
+ * @prop {string} bannerImage
+ * @prop {string} description
+ * @prop {Title} title
+ * @prop {Date} startDate
+ * @prop {Date} endDate
+ * @prop {number} episodes
+ * @prop {string[]} genres
+ *
+ */
+
+/**
+ * @return {Media}
+ */
+const media = ($page) => $page.anime.Page.media[0];
+const bannerImage = ($page) => media($page).bannerImage ?? "";
+const title = ($page) =>
+  media($page).title.english ?? media($page).title.native;
+const startYear = ($page) => media($page).startDate.year;
+const endYear = ($page) => media($page).endDate.year ?? "Current";
+const dateRange = ($page) => {
+  return `${startYear($page)} to ${endYear($page)}`;
+};
+const description = ($page) => media($page).description;
+const genres = ($page) => media($page).genres;
+</script>
+
+<template>
+  <Layout>
+    <main class="container p-0">
+      <AspectRatio w="21" h="9" class="bg-dark">
+        <img class="w-100 h-100 object-cover" :src="bannerImage($page)" />
+      </AspectRatio>
+      <section class="container">
+        <h1 class="mb-0">{{ title($page) }}</h1>
+        <p class="h6 text-muted mb-0">{{ dateRange($page) }}</p>
+        <div class="d-flex gap-2 mt-1">
+          <div
+            class="badge badge-pill badge-primary"
+            v-for="genre in genres($page)"
+            v-bind:key="genre"
+          >
+            {{ genre }}
+          </div>
+        </div>
+        <ReadMore class="text-muted mt-2" :content="description($page)" />
+      </section>
+    </main>
+  </Layout>
+</template>
+
+<style scoped>
+.gap-2 {
+  gap: calc(0.25rem * 2);
+}
+.object-cover {
+  object-fit: cover;
+}
+</style>
